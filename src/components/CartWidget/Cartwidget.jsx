095372301b1a0590c7a5585e154a4c6f51cee170
Kori Ditmeyer <kori.ditmeyer@gmail.com>
2023-12-11 00:42:36 +0900

    update index.js to solve deploy error

--- src/components/CartWidget/Cartwidget.jsx
+++ src/components/CartWidget/Cartwidget.jsx
@@ -3,26 +3,27 @@ import { CartContext } from "../../context/CartContext";
 
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
-const CartWidget = () => {
-  const { totalQuantity } = useContext(CartContext);
-
-  return (
-    <div className="sm:flex hidden justify-center items-center">
-      <div className="relative">
-        {totalQuantity ? (
-          <div className="absolute -top-1 right-0">
-            <p className="flex h-6 w-6 items-center justify-center rounded-full bg-green-500 p-2 text-xs text-white">
-              {totalQuantity}
-            </p>
-          </div>
-        ) : (
-          <div></div>
-        )}
-        <ShoppingCartIcon className="h-[40px] " />
+const Cartwidget = () => {
+    const { totalQuantity } = useContext(CartContext);
+  
+    return (
+      <div className="sm:flex hidden justify-center items-center">
+        <div className="relative">
+          {totalQuantity ? (
+            <div className="absolute -top-1 right-0">
+              <p className="flex h-6 w-6 items-center justify-center rounded-full bg-green-500 p-2 text-xs text-white">
+                {totalQuantity}
+              </p>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <ShoppingCartIcon className="h-[40px] " />
+        </div>
+        <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
       </div>
-      <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
-    </div>
-  );
-};
+    );
+  };
+  
 
-export default CartWidget;
+export default Cartwidget;
